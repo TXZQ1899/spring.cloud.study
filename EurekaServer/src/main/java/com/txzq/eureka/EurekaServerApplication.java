ea8dc8387771333b93ec958ee7577d49fcfcc0bd
Vian Tang <vian.tang@live.com>
2019-01-07 21:29:28 +0800

    override DNS Resolver

--- EurekaServer/src/main/java/com/txzq/eureka/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/txzq/eureka/EurekaServerApplication.java
@@ -9,8 +9,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @EnableEurekaClient
 public class EurekaServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+	
 }
+
+
